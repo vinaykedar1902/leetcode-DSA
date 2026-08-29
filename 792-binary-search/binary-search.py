6d2466b2716d6db9558334c2209aef21b0da0527
Vinay Kedar K <vinaykedar1902@gmail.com>
2026-08-29 16:29:40 +0530

    Time: 3 ms (5.49%) | Memory: 20.6 MB (8.85%) - LeetSync

---
+++ 792-binary-search/binary-search.py
@@ -0,0 +1,20 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n=len(nums)
+        l=0
+        r=n-1
+
+        while l<=r:
+            mid=(l+r)//2
+            if target==nums[mid]:
+                return mid
+            elif target> nums[mid]:
+                #right shift
+                l=mid+1
+            else:
+                #left shift
+                r=mid-1
+
+        return -1
+
+        
