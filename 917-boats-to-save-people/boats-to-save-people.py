070e26a30c24b2fad38e882508ee893856972145
Vinay Kedar K <vinaykedar1902@gmail.com>
2026-09-19 09:23:24 +0530

    Time: 55 ms (28.13%) | Memory: 25.4 MB (17.69%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.py
+++ 917-boats-to-save-people/boats-to-save-people.py
@@ -1,23 +1,20 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
-        m = max(people)
-        count = [0] * (m + 1)
-        for p in people:
-            count[p] += 1
-
-        idx, i = 0, 1
-        while idx < len(people):
-            while count[i] == 0:
-                i += 1
-            people[idx] = i
-            count[i] -= 1
-            idx += 1
-
-        res, l, r = 0, 0, len(people) - 1
-        while l <= r:
-            remain = limit - people[r]
-            r -= 1
-            res += 1
-            if l <= r and remain >= people[l]:
-                l += 1
-        return res
+        people.sort()
+        l = 0
+        r = len(people)-1
+        boats = 0
+        while l<r:
+            while l<r and people[r]>=limit:
+                boats+=1
+                r-=1
+            if l<r and people[l]+people[r]>limit:
+                r-=1
+                boats+=1
+            else:
+                l+=1
+                r-=1
+                boats+=1
+        if l==r:
+            boats+=1
+        return boats
